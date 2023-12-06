218952db3bf3a867410453a7b421eb8754205b63
TharadonHeebnak <84135426+TharadonHeebnak@users.noreply.github.com>
2023-12-06 13:59:43 +0700

    Revert "Revert "stage""
    This reverts commit 35cd7b4f337e45c49f7de066d43ac6dd3ca67d53.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Input, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputFileComponent } from './input-file/input-file.component';
+import { EncryptionComponent } from './encryption/encryption.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'input-file',
+    component: InputFileComponent
+  },
+  {
+    path: 'encryption',
+    component:EncryptionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
